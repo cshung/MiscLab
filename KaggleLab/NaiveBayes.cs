5eaa69b7a6f3f1bae5992039a1c09820adb894ac
Andrew Au <andrewau@microsoft.com>
2015-02-05 09:29:53 -0800

    NaiveBayes on Kaggle problem

---
+++ KaggleLab/NaiveBayes.cs
@@ -0,0 +1,121 @@
+﻿namespace KaggleLab
+{
+    using System;
+    using System.Collections.Generic;
+
+    interface IDataSet
+    {
+        int GetNumClasses();
+        int GetNumAttributes();
+        int GetNumDataVectors();
+        IDataVector GetDataVector(int i);
+        List<string> GetAttributeDomain(int attribute);
+    }
+
+    interface IDataVector
+    {
+        string GetAttributeValue(int i);
+        int GetClass();
+    }
+
+    class Model
+    {
+        public double[] prior;
+        public Dictionary<string, double>[,] likeihood;
+    }
+
+    internal static class NaiveBayes
+    {
+        internal static double Test(Model model, IDataSet testDataSet)
+        {
+            int successCount = 0;
+            double[] posteriors = new double[testDataSet.GetNumClasses()];
+            for (int i = 0; i < testDataSet.GetNumDataVectors(); i++)
+            {
+                IDataVector dataVector = testDataSet.GetDataVector(i);
+                for (int c = 0; c < testDataSet.GetNumClasses(); c++)
+                {
+                    posteriors[c] = model.prior[c];
+                    for (int j = 0; j < testDataSet.GetNumAttributes(); j++)
+                    {
+                        posteriors[c] *= model.likeihood[j, c][dataVector.GetAttributeValue(j)];
+                    }
+                }
+
+                int maxPosteriorClass = -1;
+                double maxPosterior = -1;
+                for (int c = 0; c < testDataSet.GetNumClasses(); c++)
+                {
+                    if (posteriors[c] > maxPosterior)
+                    {
+                        maxPosterior = posteriors[c];
+                        maxPosteriorClass = c;
+                    }
+                }
+                if (maxPosteriorClass == dataVector.GetClass())
+                {
+                    successCount++;
+                }
+            }
+            return ((double)successCount) / testDataSet.GetNumDataVectors();
+        }
+
+        internal static Model Train(IDataSet dataSet)
+        {
+            // Step 1: Estimate all the prior probabilities
+            double[] prior = new double[dataSet.GetNumClasses()];
+            for (int i = 0; i < dataSet.GetNumClasses(); i++)
+            {
+                prior[i] = 0;
+            }
+            for (int i = 0; i < dataSet.GetNumDataVectors(); i++)
+            {
+                IDataVector dataVector = dataSet.GetDataVector(i);
+                prior[dataVector.GetClass()]++;
+            }
+
+            // Step 2: Estimate all the likelihood values
+            // Probability(attribute_i = domain_i | class = x);
+
+            Dictionary<string, double>[,] likeihoods = new Dictionary<string, double>[dataSet.GetNumAttributes(), dataSet.GetNumClasses()];
+            for (int c = 0; c < dataSet.GetNumClasses(); c++)
+            {
+                for (int j = 0; j < dataSet.GetNumAttributes(); j++)
+                {
+                    likeihoods[j, c] = new Dictionary<string, double>();
+                    foreach (var attributeValue in dataSet.GetAttributeDomain(j))
+                    {
+                        likeihoods[j, c].Add(attributeValue, 0);
+                    }
+                }
+            }
+            for (int i = 0; i < dataSet.GetNumDataVectors(); i++)
+            {
+                IDataVector dataVector = dataSet.GetDataVector(i);
+                for (int j = 0; j < dataSet.GetNumAttributes(); j++)
+                {
+                    likeihoods[j, dataVector.GetClass()][dataVector.GetAttributeValue(j)]++;
+                }
+            }
+
+            // Step 3: Finalize probability computation by normalizing appropriately   
+            //         This is count driven maximum likelihood estimates - so it is possible for unobserved scenario to have 0 probability.
+            for (int c = 0; c < dataSet.GetNumClasses(); c++)
+            {
+                for (int j = 0; j < dataSet.GetNumAttributes(); j++)
+                {
+                    foreach (var attributeValue in dataSet.GetAttributeDomain(j))
+                    {
+                        likeihoods[j, c][attributeValue] /= prior[c];
+                    }
+                }
+            }
+            for (int i = 0; i < dataSet.GetNumClasses(); i++)
+            {
+                prior[i] /= dataSet.GetNumDataVectors();
+            }
+
+            return new Model { prior = prior, likeihood = likeihoods };
+        }
+    }
+}
---
+++ KaggleLab/Program.cs
@@ -0,0 +1,198 @@
+﻿namespace KaggleLab
+{
+    using LINQtoCSV;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Person
+    {
+        [CsvColumn(FieldIndex = 1, CanBeNull = false)]
+        public int PassengerId { get; set; }
+
+        [CsvColumn(FieldIndex = 2, CanBeNull = false)]
+        public int Survived { get; set; }
+
+        [CsvColumn(FieldIndex = 3, CanBeNull = false)]
+        public int Pclass { get; set; }
+
+        [CsvColumn(FieldIndex = 4, CanBeNull = false)]
+        public string Name { get; set; }
+
+        [CsvColumn(FieldIndex = 5, CanBeNull = false)]
+        public string Sex { get; set; }
+
+        [CsvColumn(FieldIndex = 6, CanBeNull = true)]
+        public double? Age { get; set; }
+
+        [CsvColumn(FieldIndex = 7, CanBeNull = false)]
+        public int SibSp { get; set; }
+
+        [CsvColumn(FieldIndex = 8, CanBeNull = false)]
+        public int Parch { get; set; }
+
+        [CsvColumn(FieldIndex = 9, CanBeNull = false)]
+        public string Ticket { get; set; }
+
+        [CsvColumn(FieldIndex = 10, CanBeNull = false)]
+        public double Fare { get; set; }
+
+        [CsvColumn(FieldIndex = 11, CanBeNull = true)]
+        public string Cabin { get; set; }
+
+        [CsvColumn(FieldIndex = 12, CanBeNull = true)]
+        public string Embarked { get; set; }
+    }
+
+    class PersonDataSet : IDataSet
+    {
+        Person[] persons;
+
+        public PersonDataSet(Person[] persons)
+        {
+            this.persons = persons;
+        }
+
+        public int GetNumClasses()
+        {
+            return 2;
+        }
+
+        public int GetNumAttributes()
+        {
+            return 3;
+        }
+
+        public int GetNumDataVectors()
+        {
+            return this.persons.Length;
+        }
+
+        public IDataVector GetDataVector(int i)
+        {
+            return new PersonDataVector(persons[i]);
+        }
+
+        public List<string> GetAttributeDomain(int attribute)
+        {
+            if (attribute == 0)
+            {
+                return new List<string> { "male", "female" };
+            }
+            else if (attribute == 1)
+            {
+                return new List<string> { "1", "2", "3" };
+            }
+            else if (attribute == 2)
+            {
+                return new List<string> { "0", "1", "2", "3", "4", "5", "6", "7", "8", "Null" };
+            }
+            else
+            {
+                throw new Exception("No such attribute");
+            }
+        }
+    }
+
+    class PersonDataVector : IDataVector
+    {
+        private Person person;
+
+        public PersonDataVector(Person person)
+        {
+            this.person = person;
+        }
+
+        public string GetAttributeValue(int i)
+        {
+            if (i == 0)
+            {
+                return person.Sex;
+            }
+            else if (i == 1)
+            {
+                return person.Pclass.ToString();
+            }
+            else if (i == 2)
+            {
+                if (person.Age.HasValue)
+                {
+                    return ((int)(person.Age / 10)).ToString();
+                }
+                else
+                {
+                    return "Null";
+                }
+            }
+            else
+            {
+                throw new Exception("No such attribute");
+            }
+        }
+
+        public int GetClass()
+        {
+            return person.Survived;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var persons = new CsvContext().Read<Person>(@"C:\Users\andrewau\Desktop\DesktopFiles\train.csv", new CsvFileDescription { SeparatorChar = ',', FirstLineHasColumnNames = true }).ToArray();
+
+            // Exploratory data analysis
+            // We already knew this
+
+            //double survivedFemaleCount = persons.Where(p => p.Sex.Equals("female") && p.Survived == 1).Count();
+            //double femaleCount = persons.Where(p => p.Sex.Equals("female")).Count();
+
+            //double survivedMaleCount = persons.Where(p => p.Sex.Equals("male") && p.Survived == 1).Count();
+            //double maleCount = persons.Where(p => p.Sex.Equals("male")).Count();
+
+            //Console.WriteLine(survivedFemaleCount / femaleCount);
+            //Console.WriteLine(survivedMaleCount / maleCount);
+
+            // Let's look at the data from a pclass perspective
+            // Upper class  has 62% survival rate
+            // Middle class has 47% 
+            // Lower class  has 24% 
+            // Console.WriteLine(ComputeConditionalProbability(persons, p => p.Pclass == 1));
+
+            // Let's look at the data from a sex perspective
+            // Female       has 75% 
+            // Male         has 19% 
+            // Console.WriteLine(ComputeConditionalProbability(persons, p => p.Sex.Equals("male")));
+
+            // Let's look at the data from an age perspective
+            // Average survived age = 24
+            // Average not dead age = 23
+            // Nothing real special here
+            // But if we bucket the ages
+            // Age 0 - 9   has 61% survival rate 
+            // Age 10 - 19 has 40%
+            // Age 20 - 29 has 35%
+            // Age 30 - 39 has 43%
+            // Age 40 - 49 has 38%
+            // Age 50 - 59 has 41%
+            // Age 60 - 69 has 31%
+            // Age 70 - 79 has 0%
+            // Console.WriteLine(ComputeConditionalProbability(persons, p => p.Age != null && ((int)(p.Age / 10)) == 5));
+
+            // Look like these are good features, let's try to do a simple Naive Bayes classifier on it?
+            Person[] train = persons.Take(800).ToArray();
+            Person[] test = persons.Skip(800).ToArray();
+            Model model = NaiveBayes.Train(new PersonDataSet(train));
+            Console.WriteLine(NaiveBayes.Test(model, new PersonDataSet(test)));
+        }
+
+        private static double ComputeConditionalProbability(Person[] persons, Predicate<Person> isPersonSpecial)
+        {
+            double survivedSpecialCount = persons.Where(p => isPersonSpecial(p) && p.Survived == 1).Count();
+            double specialCount = persons.Where(p => isPersonSpecial(p)).Count();
+            double conditionalProbability = survivedSpecialCount / specialCount;
+            return conditionalProbability;
+        }
+    }
+}
