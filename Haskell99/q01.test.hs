cc0665eb8a9aeefa45f433422ade8b8e858c7c88
cshung <cshung@gmail.com>
2015-06-02 06:50:53 -0700

    type annotate assert instead of deriving code under test

--- Haskell99/q01.test.hs
+++ Haskell99/q01.test.hs
@@ -1,11 +1,11 @@
 import Test.HUnit
 import Q01
 
-myLastTestInt :: [Int] -> Maybe Int
-myLastTestInt a = myLastTest a
+assertEqualMaybeInt::String -> Maybe Int -> Maybe Int -> Assertion
+assertEqualMaybeInt = assertEqual
 
-test1 = TestCase (assertEqual "myLastTest []     should be Nothing." Nothing  (myLastTestInt []    )) -- If using myLastTest, Show a is ambigouous.
-test2 = TestCase (assertEqual "myLastTest [0]    should be Just 0. " (Just 0) (myLastTest [0]      ))
-test3 = TestCase (assertEqual "myLastTest [0, 1] should be Just 1. " (Just 1) (myLastTest [0, 1]   ))
+test1 = TestCase (assertEqualMaybeInt "myLastTest []     should be Nothing." Nothing  (myLastTest []    )) 
+test2 = TestCase (assertEqual         "myLastTest [0]    should be Just 0. " (Just 0) (myLastTest [0]   ))
+test3 = TestCase (assertEqual         "myLastTest [0, 1] should be Just 1. " (Just 1) (myLastTest [0, 1]))
 
 main = runTestTT $ TestList [test1, test2, test3]
