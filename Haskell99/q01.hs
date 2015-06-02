575bc116c39033e8492418aa630787268cc14c90
cshung <cshung@gmail.com>
2015-06-01 20:32:48 -0700

    Haskell updates

--- Haskell99/q01.hs
+++ Haskell99/q01.hs
@@ -1,7 +1,10 @@
 module Q01 where
 
 myLast :: [a] -> a
-myLast (x:[]) = x
-myLast []     = error "Wrong"
-myLast (_:t)  = myLast(t)
+myLast y = case myLastTest(y) of Just x  -> x
+                                 Nothing -> error "Cannot obtain myLast of an empty list"
 
+myLastTest :: [a] -> Maybe a
+myLastTest (x:[])  = Just x
+myLastTest  []     = Nothing
+myLastTest  (_:t)  = myLastTest(t)
--- Haskell99/q01.test.hs
+++ Haskell99/q01.test.hs
@@ -1,7 +1,11 @@
 import Test.HUnit
 import Q01
 
-test1 = TestCase (assertEqual "myLast [0] should be 0." 0 (myLast [0]))
-test2 = TestCase (assertEqual "myLast [0, 1] should be 1"  1 (myLast [0, 1]))
+myLastTestInt :: [Int] -> Maybe Int
+myLastTestInt a = myLastTest a
 
-main = runTestTT $ TestList [test1, test2]
+test1 = TestCase (assertEqual "myLastTest []     should be Nothing." Nothing  (myLastTestInt []    )) -- If using myLastTest, Show a is ambigouous.
+test2 = TestCase (assertEqual "myLastTest [0]    should be Just 0. " (Just 0) (myLastTest [0]      ))
+test3 = TestCase (assertEqual "myLastTest [0, 1] should be Just 1. " (Just 1) (myLastTest [0, 1]   ))
+
+main = runTestTT $ TestList [test1, test2, test3]
