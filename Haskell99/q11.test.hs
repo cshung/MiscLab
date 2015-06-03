c00f787482959c3cc0d03761d00cd4ffbb8d4a25
Andrew Au <cshung@gmail.com>
2015-06-03 15:08:57 -0700

    whitespace change only

--- Haskell99/q11.test.hs
+++ Haskell99/q11.test.hs
@@ -10,8 +10,5 @@ test3 = TestCase (assertEqualEntryList "encodeModified [1,2]   should be [(Singl
 test4 = TestCase (assertEqualEntryList "encodeModified [1,2,1] should be [(Single 1),(Single 2),(Single 1)]." ([(Single 1),(Single 2),(Single 1)]) (encodeModified [1,2,1]))
 test5 = TestCase (assertEqualEntryList "encodeModified [1,1,2] should be [(Multiple 2 1),(Single 2)]       ." ([(Multiple 2 1),(Single 2)]       ) (encodeModified [1,1,2]))
 test6 = TestCase (assertEqualEntryList "encodeModified [1,2,2] should be [(Single 1),(Multiple 2 2)]       ." ([(Single 1),(Multiple 2 2)]       ) (encodeModified [1,2,2]))
-                                                                                  
-main = runTestTT $ TestList [test1,test2,test3,test4,test5,test6]                 
-                                                                                  
-                                                                                  
-                                                                                  
+
+main = runTestTT $ TestList [test1,test2,test3,test4,test5,test6]
--- Haskell99/q12.test.hs
+++ Haskell99/q12.test.hs
@@ -11,8 +11,5 @@ test3 = TestCase (assertEqualIntList "decodeModified [(Single 1),(Single 2)]    
 test4 = TestCase (assertEqualIntList "decodeModified [(Single 1),(Single 2),(Single 1)] should be [1,2,1]." ([1,2,1]) (decodeModified [(Single 1),(Single 2),(Single 1)]))
 test5 = TestCase (assertEqualIntList "decodeModified [(Multiple 2 1),(Single 2)]        should be [1,1,2]." ([1,1,2]) (decodeModified [(Multiple 2 1),(Single 2)]       ))
 test6 = TestCase (assertEqualIntList "decodeModified [(Single 1),(Multiple 2 2)]        should be [1,2,2]." ([1,2,2]) (decodeModified [(Single 1),(Multiple 2 2)]       ))
-                                                                                  
-main = runTestTT $ TestList [test1,test2,test3,test4,test5,test6]                 
-                                                                                  
-                                                                                  
-                                                                                  
+
+main = runTestTT $ TestList [test1,test2,test3,test4,test5,test6]
