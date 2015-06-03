3b040a8966ede010726f22f87254dd55bcf96d29
cshung <cshung@gmail.com>
2015-06-02 18:56:20 -0700

    no longer useful

--- Haskell99/test.bat
+++
@@ -1,5 +0,0 @@
-type 01.test.txt | ghci > 01.test.actual 2>&1
-type 02.test.txt | ghci > 02.test.actual 2>&1
-type 03.test.txt | ghci > 03.test.actual 2>&1
-type 04.test.txt | ghci > 04.test.actual 2>&1
-type 05.test.txt | ghci > 05.test.actual 2>&1
