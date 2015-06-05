9922a25d8ac61fb1d238f6e6f11968465e921b38
cshung <cshung@gmail.com>
2015-06-04 21:04:10 -0700

    Improve Haskell 99 Problem 7

--- Haskell99/q07.hs
+++ Haskell99/q07.hs
@@ -2,5 +2,12 @@ module Q07 where
 
 data NestedList a = Elem a | List [NestedList a]
 
-flatten (Elem x)  = [x]
-flatten (List y) = foldr (++) [] $ map flatten y
+flatten :: NestedList a -> [a]
+flatten x =
+  let
+    flatten' :: NestedList a -> [a] -> [a]    
+    flatten' (Elem x)     acc = x:acc
+    flatten' (List [] )   acc = acc
+    flatten' (List (p:q)) acc = flatten' p (flatten' (List q) acc)
+  in
+    flatten' x []
