883ba984df2d2c3ceb9a26b98215746ef6d8b8ac
cshung <cshung@gmail.com>
2015-06-05 06:00:48 -0700

    Difference List in action!

--- Haskell99/q07.hs
+++ Haskell99/q07.hs
@@ -3,11 +3,10 @@ module Q07 where
 data NestedList a = Elem a | List [NestedList a]
 
 flatten :: NestedList a -> [a]
-flatten x =
+flatten x = 
   let
-    flatten' :: NestedList a -> [a] -> [a]    
-    flatten' (Elem x)     acc = x:acc
-    flatten' (List [] )   acc = acc
-    flatten' (List (p:q)) acc = flatten' p (flatten' (List q) acc)
+    flatten' :: NestedList a -> [a] -> [a]  
+    flatten' (Elem x)  = \y -> x:y
+    flatten' (List y) = foldr (.) (\x -> []) $ map flatten' y
   in
-    flatten' x []
+    (flatten' x) []
