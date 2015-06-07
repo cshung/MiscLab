1dd3227652d36c80526c6bb756fd63659f888881
cshung <cshung@gmail.com>
2015-06-07 13:24:11 -0700

    debugged AVL tree!

--- Haskell99/temp.hs
+++ Haskell99/temp.hs
@@ -11,7 +11,7 @@ getSize Node { size = size } = size
 buildTree :: Tree a -> a -> Tree a -> Tree a
 buildTree left x right = Node {value = x, size = (getSize left) + (getSize right) + 1, height = (max (getHeight left) (getHeight right)) + 1, left = left, right = right}
 
-insert :: (Ord a) => Tree a -> a -> Tree a
+insert :: (Show a, Ord a) => Tree a -> a -> Tree a
 insert Empty x = Node { value = x, size = 1, height = 1, left = Empty, right = Empty }
 insert Node { value = value, left = left, right = right } x =
   if x > value then
@@ -19,8 +19,9 @@ insert Node { value = value, left = left, right = right } x =
   else
     balance (buildTree (insert left x) value right)
 
-balance :: Tree a -> Tree a
-balance x =
+balance :: (Show a) => Tree a -> Tree a
+balance Empty = Empty
+balance x     =
   let
     getBalance :: Tree a -> Int
     getBalance x = (getHeight (right x)) - (getHeight (left x))
@@ -37,8 +38,9 @@ balance x =
               rotateRight (buildTree (rotateLeft (left x)) (value x) (right x))
             else
               rotateRight x
+      otherwise -> error ("Balance failed - the subtree is \n" ++ (preOrder x 0))
 
-rotateLeft :: Tree a -> Tree a
+rotateLeft :: (Show a) => Tree a -> Tree a
 rotateLeft oldTop =
   let
     Node { value = oldTopValue  , left = oldTopLeft  , right = oldTopRight   } = oldTop       -- to rotate left, top   must be non empty
@@ -47,7 +49,7 @@ rotateLeft oldTop =
   in
    buildTree newLeft oldRightValue oldRightRight
 
-rotateRight :: Tree a -> Tree a
+rotateRight :: (Show a) => Tree a -> Tree a
 rotateRight oldTop =
   let
     Node { value = oldTopValue , left = oldTopLeft , right = oldTopRight  } = oldTop      -- to rotate right, top  must be non empty
@@ -56,7 +58,7 @@ rotateRight oldTop =
   in
    buildTree oldLeftLeft oldLeftValue newRight
 
-select :: Tree a -> Int -> a
+select :: (Show a) => Tree a -> Int -> a
 select Empty _ = error "Unexpected"
 select Node { value = value, size = size, left = left, right = right } index
   | index < split  = select left index
@@ -64,27 +66,31 @@ select Node { value = value, size = size, left = left, right = right } index
   | otherwise      = select right (index - split)
   where split = (getSize left) + 1
 
-deleteByIndex :: Tree a -> Int -> Tree a
+deleteByIndex :: (Show a) => Tree a -> Int -> (a, Tree a)
 deleteByIndex Empty _ = error "Unexpected"
 deleteByIndex node index
-  | index < split  = balance (buildTree (deleteByIndex nodeLeft index) nodeValue nodeRight)
-  | index == split = deleteRootNode node
-  | otherwise      = balance (buildTree nodeLeft nodeValue (deleteByIndex nodeRight (index - split)))
+  | index < split  = let (deletedValue, deletedLeft ) = (deleteByIndex nodeLeft index)            in (deletedValue, balance (buildTree deletedLeft nodeValue nodeRight))
+  | index == split = (nodeValue, deleteRootNode node)
+  | otherwise      = let (deletedValue, deletedRight) = (deleteByIndex nodeRight (index - split)) in (deletedValue, balance (buildTree nodeLeft nodeValue deletedRight))
   where nodeValue = value node
         nodeLeft  = left node
         nodeRight = right node
         split     = (getSize nodeLeft) + 1
 
-deleteRootNode :: Tree a -> Tree a
+deleteRootNode :: (Show a) => Tree a -> Tree a
 deleteRootNode Empty = error "Unexpected"
 deleteRootNode Node {left = Empty, right = right} = right
 deleteRootNode Node {left = left,  right = Empty} = left
 deleteRootNode node = balance (buildTree (left node) firstValue tree) where (firstValue, tree) = deleteFirst (right node)
 
-deleteFirst :: Tree a -> (a, Tree a)
+deleteFirst :: (Show a) => Tree a -> (a, Tree a)
 deleteFirst Node {left = Empty, value = value, right = right} = (value, right)
-deleteFirst Node {left = left, value = _, right = _} = (resultValue, balance resultTree) where (resultValue, resultTree) = (deleteFirst left)
+deleteFirst Node {left = left, value = value, right = right} = (resultValue, balance (buildTree resultTree value right)) where (resultValue, resultTree) = (deleteFirst left)
 
-inOrder :: Tree a -> [a]
-inOrder Empty = []
-inOrder Node { value = value, left = left, right = right } = (inOrder left) ++ value: (inOrder right)
+indent :: Int -> String
+indent 0 = ""
+indent x = ' ':(indent (x - 1))
+
+preOrder :: (Show a) => Tree a -> Int -> String
+preOrder Empty x = indent x ++ "(null)\n"
+preOrder Node { value = value, size = size, height = height, left = left, right = right } x = indent x ++ (show value) ++ "(size = " ++ (show size) ++ ", height = " ++ (show height) ++ ")\n" ++ (preOrder left (x + 2)) ++ (preOrder right (x + 2))
