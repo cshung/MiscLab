6d92b8031ddb99c7cdc8e7745197970e0a5a8f04
cshung <cshung@gmail.com>
2015-06-13 09:16:19 -0700

    More AVL map operations

--- Haskell99/AvlMap.hs
+++ Haskell99/AvlMap.hs
@@ -1,6 +1,22 @@
-module AvlMap where
+module AvlMap
+(
+  Tree(),
+  empty,
+  insert,
+  containsKey,
+  valueByKey,
+  setValueByKey,
+  deleteByKey,
+  valueByIndex,
+  setValueByIndex,
+  deleteByIndex
+)
+where
 
 data Tree tKey tValue = Empty | Node { key :: tKey, value :: tValue, size :: Int, height :: Int, left :: Tree tKey tValue, right :: Tree tKey tValue}
+
+empty :: Tree tKey tValue
+empty = Empty
 
 getHeight :: Tree tKey tValue -> Int
 getHeight Empty = 0
@@ -34,14 +50,6 @@ balance x =
               rotateRight x
       otherwise -> error "This is impossible"
 
-insert :: (Ord tKey) => Tree tKey tValue -> tKey -> tValue -> Tree tKey tValue
-insert Empty key value = Node { key = key, value = value, size = 1, height = 1, left = Empty, right = Empty }
-insert Node { key = nodeKey, value = nodeValue, left = left, right = right } insertKey insertValue =
-  if insertKey > nodeKey then
-    balance (buildTree left nodeKey nodeValue (insert right insertKey insertValue))
-  else
-    balance (buildTree (insert left insertKey insertValue) nodeKey nodeValue right)
-
 rotateLeft :: Tree tKey tValue -> Tree tKey tValue
 rotateLeft oldTop =
   let
@@ -60,28 +68,72 @@ rotateRight oldTop =
   in
    buildTree oldLeftLeft oldLeftKey oldLeftValue newRight
 
-deleteByKey :: (Ord tKey) => Tree tKey tValue -> tKey -> (tKey, tValue, Tree tKey tValue)
-deleteByKey Empty _ = error "Unexpected 3"
-deleteByKey node deleteKey
-  | deleteKey <  nodeKey  = let (deletedKey, deletedValue, deletedLeft ) = (deleteByKey nodeLeft deleteKey) in (deletedKey, deletedValue, balance (buildTree deletedLeft nodeKey nodeValue nodeRight))
-  | deleteKey == nodeKey  = (nodeKey, nodeValue, deleteRootNode node)
-  | otherwise             = let (deletedKey, deletedValue, deletedRight) = (deleteByKey nodeRight deleteKey) in (deletedKey, deletedValue, balance (buildTree nodeLeft nodeKey nodeValue deletedRight))
+insert :: (Ord tKey) => Tree tKey tValue -> tKey -> tValue -> Maybe (Tree tKey tValue)
+insert Empty key value = Just Node { key = key, value = value, size = 1, height = 1, left = Empty, right = Empty }
+insert Node { key = nodeKey, value = nodeValue, left = left, right = right } insertKey insertValue =
+  if insertKey > nodeKey then
+    case insert right insertKey insertValue of Just insertedRight -> Just (balance (buildTree left nodeKey nodeValue insertedRight))
+                                               Nothing            -> Nothing
+  else
+    if insertKey == nodeKey then
+      Nothing
+    else
+      case insert left insertKey insertValue of Just insertedLeft -> Just (balance (buildTree insertedLeft nodeKey nodeValue right))
+                                                Nothing           -> Nothing
+
+byKey :: (Ord tKey) => Tree tKey tValue -> tKey -> (Tree tKey tValue -> (b, Tree tKey tValue)) -> Maybe (b, Tree tKey tValue)
+byKey Empty _        _               = Nothing
+byKey node searchKey processFunction
+  | searchKey <  nodeKey  = case (byKey nodeLeft  searchKey processFunction) of (Just (processResult, processedLeft )) -> Just (processResult, balance (buildTree processedLeft nodeKey nodeValue nodeRight))
+                                                                                Nothing                                -> Nothing
+  | searchKey >  nodeKey  = case (byKey nodeRight searchKey processFunction) of (Just (processResult, processedRight)) -> Just (processResult, balance (buildTree nodeLeft nodeKey nodeValue processedRight))
+                                                                                Nothing                                -> Nothing
+  | searchKey == nodeKey  = Just (processFunction node)
+  | otherwise             = error "This is impossible"
   where nodeKey   = key node
         nodeValue = value node
         nodeLeft  = left node
         nodeRight = right node
 
-deleteByIndex :: Tree tKey tValue -> Int -> (tKey, tValue, Tree tKey tValue)
-deleteByIndex Empty _ = error "Unexpected 1"
-deleteByIndex node index
-  | index < split  = let (deletedKey, deletedValue, deletedLeft ) = (deleteByIndex nodeLeft index)            in (deletedKey, deletedValue, balance (buildTree deletedLeft nodeKey nodeValue nodeRight))
-  | index == split = (nodeKey, nodeValue, deleteRootNode node)
-  | otherwise      = let (deletedKey, deletedValue, deletedRight) = (deleteByIndex nodeRight (index - split)) in (deletedKey, deletedValue, balance (buildTree nodeLeft nodeKey nodeValue deletedRight))
+valueByKey :: (Ord tKey) => Tree tKey tValue -> tKey -> Maybe tValue
+valueByKey node searchKey = case (byKey node searchKey (\foundNode -> (value foundNode, foundNode))) of Just (foundValue, _) -> Just foundValue
+                                                                                                        Nothing              -> Nothing
+
+containsKey:: (Ord tKey) => Tree tKey tValue -> tKey -> Bool
+containsKey node searchKey = case (valueByKey node searchKey) of Just _  -> True
+                                                                 Nothing -> False
+
+setValueByKey :: (Ord tKey) => Tree tKey tValue -> tKey -> tValue -> Maybe (Tree tKey tValue)
+setValueByKey node searchKey replaceValue = case (byKey node searchKey (\foundNode -> ((), buildTree (left foundNode) (key foundNode) replaceValue (right foundNode)))) of Just (_,result) -> Just result
+                                                                                                                                                                           Nothing         -> Nothing
+
+deleteByKey :: (Ord tKey) => Tree tKey tValue -> tKey -> Maybe ((tKey, tValue), Tree tKey tValue)
+deleteByKey node deleteKey = byKey node deleteKey (\foundNode -> ((key foundNode, value foundNode), deleteRootNode foundNode))
+
+byIndex :: Tree tKey tValue -> Int -> (Tree tKey tValue -> (b, Tree tKey tValue)) -> Maybe (b, Tree tKey tValue)
+byIndex Empty _    _               = Nothing
+byIndex node index processFunction
+  | index < split  = case (byIndex nodeLeft index            processFunction) of Just (processedResult, processedLeft ) -> Just (processedResult, balance (buildTree processedLeft nodeKey nodeValue nodeRight))
+                                                                                 Nothing                                -> Nothing
+  | index > split  = case (byIndex nodeRight (index - split) processFunction) of Just (processedResult, processedRight) -> Just (processedResult, balance (buildTree nodeLeft nodeKey nodeValue processedRight))
+                                                                                 Nothing                                -> Nothing
+  | index == split = Just (processFunction node)
   where nodeKey   = key node
         nodeValue = value node
         nodeLeft  = left node
         nodeRight = right node
         split     = (getSize nodeLeft) + 1
+
+deleteByIndex :: Tree tKey tValue -> Int -> Maybe ((tKey, tValue), Tree tKey tValue)
+deleteByIndex node deleteIndex = byIndex node deleteIndex (\foundNode -> ((key node, value node), deleteRootNode foundNode))
+
+valueByIndex ::  Tree tKey tValue -> Int -> Maybe tValue
+valueByIndex node searchIndex = case (byIndex node searchIndex (\foundNode -> (value foundNode, foundNode))) of Just (result, _) -> Just result
+                                                                                                                Nothing          -> Nothing
+
+setValueByIndex :: Tree Int tValue -> Int -> tValue -> Maybe (Tree Int tValue)
+setValueByIndex node searchIndex replaceValue = case (byIndex node searchIndex (\foundNode -> ((), buildTree (left foundNode) (key foundNode) replaceValue (right foundNode)))) of Just (_,result) -> Just result
+                                                                                                                                                                                   Nothing         -> Nothing
 
 deleteRootNode :: Tree tKey tValue -> Tree tKey tValue
 deleteRootNode Empty = error "Unexpected 2"
@@ -92,40 +144,6 @@ deleteRootNode node = balance (buildTree (left node) firstKey firstValue tree) w
 deleteFirst :: Tree tKey tValue -> (tKey, tValue, Tree tKey tValue)
 deleteFirst Node {left = Empty, key = key, value = value, right = right} = (key, value, right)
 deleteFirst Node {left = left, key = key, value = value, right = right} = (resultKey, resultValue, balance (buildTree resultTree key value right)) where (resultKey, resultValue, resultTree) = (deleteFirst left)
-
-valueByKey :: (Ord tKey) => Tree tKey tValue -> tKey -> Maybe tValue
-valueByKey Empty _           = Nothing
-valueByKey node  searchKey    =
-  let
-    Node { key = nodeKey, value = nodeValue, left = nodeLeft, right = nodeRight } = node
-  in
-    if (searchKey == nodeKey) then
-      Just nodeValue
-    else
-      if (searchKey < nodeKey) then
-        valueByKey nodeLeft searchKey
-      else
-        valueByKey nodeRight searchKey
-
-containsKey:: (Ord tKey) => Tree tKey tValue -> tKey -> Bool
-containsKey node searchKey = case (valueByKey node searchKey) of Just _  -> True
-                                                                 Nothing -> False
-
-setValueByKey :: (Ord tKey) => Tree tKey tValue -> tKey -> tValue -> Either (Tree tKey tValue) String
-setValueByKey Empty _         _             = Right "Value not found"
-setValueByKey node  searchKey replaceValue  =
-  let
-    Node { key = nodeKey, value = nodeValue, left = nodeLeft, right = nodeRight } = node
-  in
-    if (searchKey == nodeKey) then
-      Left (buildTree nodeLeft nodeKey replaceValue nodeRight)
-    else
-      if (searchKey < nodeKey) then
-        case (setValueByKey nodeLeft searchKey replaceValue)  of Left  replacedLeft  -> Left (buildTree replacedLeft nodeKey replaceValue nodeRight)
-                                                                 Right message       -> Right message
-      else
-        case (setValueByKey nodeRight searchKey replaceValue) of Left  replacedRight -> Left (buildTree nodeLeft nodeKey replaceValue replacedRight)
-                                                                 Right message       -> Right message
 
 toList :: Tree tKey tValue -> [(tKey, tValue)]
 toList Empty = []
