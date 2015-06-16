0fff485aed5a4cb42e56c26386d8c0c478dff180
cshung <cshung@gmail.com>
2015-06-15 20:59:33 -0700

    Save

--- Haskell99/temp.hs
+++ Haskell99/temp.hs
@@ -1,10 +1,7 @@
 import qualified AvlMap
-import qualified BionmialHeap
+import qualified BinomialHeap
 
 let numSignals = 5
-
 let nand  = 1
-let input0 = empty
-let input1 = empty
-let input0 = empty
 let gates = [(nand, 1, 1, 3), (nand, 2, 2, 4), (nand, 3, 4, 5)]
+let inputs = foldr (\(x,y) z -> AvlMap.insert z x y) empty [("a", 1),("b", 2)]
