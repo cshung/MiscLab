e4606d8edfb1abd427466e46a4ff9d0a1a469e1f
Andrew Au <cshung@gmail.com>
2015-06-22 17:30:32 -0700

    Compressed Trie implementation

--- SuffixTree/CompressedTrie.cpp
+++ SuffixTree/CompressedTrie.cpp
@@ -1,4 +1,5 @@
 #include "CompressedTrie.h"
+#include <algorithm>
 
 CompressedTrie::CompressedTrie() : m_root(new CompressedTrie::CompressedTrieNode())
 {
@@ -38,10 +39,84 @@ CompressedTrie::CompressedTrieEdge::~CompressedTrieEdge()
 
 bool CompressedTrie::Add(const string key, void* value)
 {
-    return false;
+    CompressedTrie::CompressedTrieNode* currentNode = this->m_root;
+    int keyCursor = 0;
+    while (keyCursor < key.length())
+    {
+        char currentCharacter = key[keyCursor];
+        map<char, CompressedTrie::CompressedTrieEdge*>::iterator probe = currentNode->m_children.find(currentCharacter);
+        if (probe == currentNode->m_children.end())
+        {
+            CompressedTrieEdge* newEdge = new CompressedTrieEdge();
+            CompressedTrieNode* newLeaf = new CompressedTrieNode();
+            newEdge->m_edgeLabel = key.substr(keyCursor);
+            newEdge->m_child = newLeaf;
+            newLeaf->m_value = value;
+            currentNode->m_children.insert(pair<char, CompressedTrie::CompressedTrieEdge*>(currentCharacter, newEdge));
+            return true;
+        }
+        else
+        {
+            CompressedTrieEdge* currentEdge = probe->second;
+            int limit = min(key.length() - keyCursor, currentEdge->m_edgeLabel.length());
+            for (int edgeCursor = 0; edgeCursor < limit; keyCursor++, edgeCursor++)
+            {
+                if (key[keyCursor] != currentEdge->m_edgeLabel[edgeCursor])
+                {
+                    // Break the current edge, add the data in, and be done with it
+                    string edgePrefix = currentEdge->m_edgeLabel.substr(0, edgeCursor);
+                    string edgeSuffix = currentEdge->m_edgeLabel.substr(edgeCursor);
+                    currentEdge->m_edgeLabel = edgePrefix;
+
+                    CompressedTrieNode* newNode = new CompressedTrieNode();
+                    CompressedTrieEdge* oldEdge = new CompressedTrieEdge();
+                    CompressedTrieEdge* newEdge = new CompressedTrieEdge();
+                    CompressedTrieNode* newLeaf = new CompressedTrieNode();
+
+                    oldEdge->m_edgeLabel = edgeSuffix;
+                    oldEdge->m_child = currentEdge->m_child;
+                    currentEdge->m_child = newNode;
+
+                    newEdge->m_edgeLabel = key.substr(keyCursor);
+                    newEdge->m_child = newLeaf;
+
+                    newNode->m_children.insert(pair<char, CompressedTrie::CompressedTrieEdge*>(edgeSuffix[0], oldEdge));
+                    newNode->m_children.insert(pair<char, CompressedTrie::CompressedTrieEdge*>(key[keyCursor], newEdge));
+
+                    newLeaf->m_value = value;
+                    return true;
+                }
+            }
+            currentNode = currentEdge->m_child;
+        }
+    }
 }
 
 void* CompressedTrie::Get(const string key) const
 {
-    return nullptr;
+    CompressedTrie::CompressedTrieNode* currentNode = this->m_root;
+    int keyCursor = 0;
+    while (keyCursor < key.length())
+    {
+        char currentCharacter = key[keyCursor];
+        map<char, CompressedTrie::CompressedTrieEdge*>::iterator probe = currentNode->m_children.find(currentCharacter);
+        if (probe == currentNode->m_children.end())
+        {
+            return nullptr;
+        }
+        else
+        {
+            CompressedTrieEdge* currentEdge = probe->second;
+            int limit = min(key.length() - keyCursor, currentEdge->m_edgeLabel.length());
+            for (int edgeCursor = 0; edgeCursor < limit; keyCursor++, edgeCursor++)
+            {
+                if (key[keyCursor] != currentEdge->m_edgeLabel[edgeCursor])
+                {
+                    return nullptr;
+                }
+            }
+            currentNode = currentEdge->m_child;
+        }
+    }
+    return currentNode->m_value;
 }
