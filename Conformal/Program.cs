cda6c62bbcd4af0b336efc6b68d5f46cdf089857
Andrew Au <cshung@gmail.com>
2015-12-24 17:04:38 -0800

    Some programs

---
+++ Conformal/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Conformal
+{
+    class Program
+    {
+        static Complex Integrand(Complex input)
+        {
+            // [(w - 250)(w + 250)]^(-2/3)
+            Complex inside = (input - 250) * (input + 250);
+            return Complex.Pow(inside, -2.0 / 3);
+        }
+
+        static void Main(string[] args)
+        {
+            //for (int real = -100; real <= 100; real += 10)
+            //{
+            //    int real = 0;
+            //    Complex result = new Complex(0, 0);
+            //    for (int imag = 0; imag < 100; imag++)
+            //    {
+            //        Complex input = new Complex(real + 0.1, imag + 0.1);
+            //        result += Integrand(input);
+            //        Console.WriteLine("{0}, {1}", result.Real, result.Imaginary);
+            //    }
+            //}
+            //for (int imag = 0; imag <= 100; imag += 10)
+            {
+                int imag = 10;
+                Complex result = new Complex(0, 0);
+                for (int real = -5000; real < 5000; real++)
+                {
+                    // Bugbug: The numerical integration must base itself from 0!
+                    Complex input = new Complex(real/10.0 + 0.1, imag + 0.1);
+                    result += Integrand(input) * 0.1;
+                    Console.WriteLine("{0}, {1}", result.Real, result.Imaginary);
+                }
+            }
+        }
+    }
+}
---
+++ SamplingWithoutReplacement/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamplingWithoutReplacement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numSamples = 5;
+            int numProblem = 17;
+
+            var problem = Enumerable.Range(1, numProblem).ToList();
+            var samples = new List<int>();
+            Random random = new Random();
+            for (int i = 0; i < numSamples; i++)
+            {
+                int id = random.Next(problem.Count());
+                samples.Add(problem[id]);
+                problem.RemoveAt(id);
+            }
+            samples.Sort();
+            Console.WriteLine(string.Join(",", samples));
+        }
+    }
+}
