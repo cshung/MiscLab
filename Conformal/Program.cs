e3c53ca16867e786c74c5bfcb22db598ce12c600
Andrew Au <cshung@gmail.com>
2015-12-25 08:28:20 -0800

    More comments

--- Conformal/Program.cs
+++ Conformal/Program.cs
@@ -1,22 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Numerics;
-
-namespace Conformal
+﻿namespace Conformal
 {
-    class Program
+    using System;
+    using System.Collections.Generic;
+    using System.Numerics;
+
+    internal static class Program
     {
-        static Complex Integrand(Complex input)
+        private static Complex Integrand(Complex input)
         {
             // [(w - 50)(w + 50)]^(-2/3)
             Complex inside = (input - 50) * (input + 50);
-            return Complex.Pow(inside, -2.0/3.0);
+            return Complex.Pow(inside, -2.0 / 3.0);
         }
 
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             // This avoid evaluating the integrand at the poles
             double shift = 0.1;
@@ -28,10 +25,13 @@ namespace Conformal
             // The base for the integration is (-100, 0)
             // We create a grid with real = (-100, 100), imag = (0, 200)
             // Using delta = 0.1
+            // The numerical integration is done using trapezoidal rule
+            // The initial and final 0.5 is omitted for simplicity
 
             // There is still some bugs, but not sure what is that ...
 
             // First, build the X and Y axis
+            // I am pretty sure the xMarks is correct as it does plot the initial triangle
             var xMarks = new List<Tuple<Complex, double>>();
             {
                 Complex current = new Complex(0, 0);
@@ -67,6 +67,8 @@ namespace Conformal
             }
 
             // Step 2: Build the grid lines
+            // Stepping upwards from the previously built xMarks
+            // This lines are really odd
             foreach (var xMark in xMarks)
             {
                 double real = xMark.Item2;
@@ -80,6 +82,8 @@ namespace Conformal
                 }
             }
 
+            // Stepping forward from the previously built yMarks
+            // This lines does show a pattern, but I am not sure why they intersect themselves
             foreach (var yMark in yMarks)
             {
                 double imag = yMark.Item2;
