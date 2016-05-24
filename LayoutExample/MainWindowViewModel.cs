83f7c7c05fa08ae94f438308966fe80a65a9acfa
Andrew Au <cshung@gmail.com>
2016-05-24 10:52:08 -0700

    WPF layout study

---
+++ LayoutExample/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace LayoutExample
+{
+    internal class MainWindowViewModel
+    {
+        public MainWindowViewModel()
+        {
+        }
+
+        public List<string> Data
+        {
+            get
+            {
+                List<string> result = new List<string>();
+                for (int i =0; i < 100; i++)
+                {
+                    result.Add(i + "");
+                }
+                return result;
+            }
+        }
+    }
+}
---
+++ LayoutExample/TilesPanel.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace LayoutExample
+{
+    public sealed class TilesPanel : Panel
+    {
+        int margin = 20;
+        private Nullable<Size> arrangeSize;
+
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            Size s = base.MeasureOverride(availableSize);
+            if (arrangeSize.HasValue)
+            {
+                double totalWidth = arrangeSize.Value.Width;
+                double x = margin;
+                double y = margin;
+                bool first = true;
+                double maxHeight = 0;
+
+                foreach (FrameworkElement element in this.Children)
+                {
+                    element.Measure(availableSize);
+                    double begin = x;
+                    x += element.DesiredSize.Width;
+                    x += margin;
+                    if (x < totalWidth || first)
+                    {
+                        maxHeight = Math.Max(maxHeight, element.DesiredSize.Height);
+                    }
+                    first = false;
+                    if (x >= totalWidth)
+                    {
+                        first = true;
+                        x = margin;
+                        y = y + maxHeight + margin;
+                    }
+                }
+
+                return new Size(0, y + maxHeight + margin);
+            }
+            else
+            {
+                return s;
+            }
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            if (arrangeSize.HasValue)
+            {
+                arrangeSize = null;
+                // Size and position the child elements
+                double totalWidth = finalSize.Width;
+                double x = margin;
+                double y = margin;
+                bool first = true;
+                double maxHeight = 0;
+
+                foreach (FrameworkElement element in this.Children)
+                {
+                    double begin = x;
+                    x += element.DesiredSize.Width;
+                    x += margin;
+                    if (x < totalWidth || first)
+                    {
+                        element.Arrange(new Rect(begin, y, element.DesiredSize.Width, element.DesiredSize.Height));
+                        maxHeight = Math.Max(maxHeight, element.DesiredSize.Height);
+                    }
+                    first = false;
+                    if (x >= totalWidth)
+                    {
+                        first = true;
+                        x = margin;
+                        y = y + maxHeight + margin;
+                    }
+                }
+
+                return new Size(finalSize.Width, Math.Max(finalSize.Height, y));
+            }
+            else
+            {
+                this.arrangeSize = finalSize;
+                this.InvalidateMeasure();
+                return finalSize;
+            }
+        }
+    }
+}
