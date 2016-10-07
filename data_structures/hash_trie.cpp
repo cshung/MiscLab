2094806e4d9b408839ed7279c25109cc314075a3
Andrew Au <cshung@gmail.com>
2016-10-06 19:50:08 -0700

    Hash Trie - should be correct insertion

--- data_structures/hash_trie.cpp
+++ data_structures/hash_trie.cpp
@@ -18,11 +18,13 @@ bool hash_trie::set(const char* key, int value)
 	int hash = this->hash(key);	
 	trie_node* new_root;
 	bool succeed = this->insert_trie(key, value, hash, 32, this->m_root, &new_root);
-	if (succeed)
+	if (!succeed)
 	{
-		// optionally, we can keep the old root for persistence
-		this->m_root = (hash_trie::trie_node*)new_root;
+		return false;
 	}
+	// optionally, we can keep the old root for persistence
+	this->m_root = (hash_trie::trie_node*)new_root;
+	return true;
 }
 
 int hash_trie::hash(const char* key)
@@ -46,16 +48,20 @@ bool hash_trie::insert_trie(const char* key, int value, int hash, int num_bits, 
 	node* result;
 	trie_node* trie_result;
 	bucket_node* bucket_result;
+	trie_node* left_trie_node = (trie_node*)((current == nullptr) ? nullptr : current->m_left);
+	trie_node* right_trie_node = (trie_node*)((current == nullptr) ? nullptr : current->m_right);
+	bucket_node* left_bucket_node = (bucket_node*)((current == nullptr) ? nullptr : current->m_left);
+	bucket_node* right_bucket_node = (bucket_node*)((current == nullptr) ? nullptr : current->m_right);
 	if (bit == 0)
 	{
 		if (num_bits > 1)
 		{
-			succeed = insert_trie(key, value, hash >> 1, num_bits - 1, (trie_node*)current->m_left, &trie_result);
+			succeed = insert_trie(key, value, hash >> 1, num_bits - 1, left_trie_node, &trie_result);
 			result = trie_result;
 		}
 		else
 		{
-			succeed = insert_bucket(key, value, (bucket_node*)current->m_left, &bucket_result);
+			succeed = insert_bucket(key, value, left_bucket_node, &bucket_result);
 			result = bucket_result;
 		}
 		if (!succeed)
@@ -64,18 +70,18 @@ bool hash_trie::insert_trie(const char* key, int value, int hash, int num_bits, 
 		}
 		*ppResult = new trie_node();
 		(*ppResult)->m_left = result;
-		(*ppResult)->m_right= current->m_right;
+		(*ppResult)->m_right= right_trie_node;
 	}
 	else
 	{
 		if (num_bits > 1)
 		{
-			succeed = insert_trie(key, value, hash >> 1, num_bits - 1, (trie_node*)current->m_left, &trie_result);
+			succeed = insert_trie(key, value, hash >> 1, num_bits - 1, right_trie_node, &trie_result);
 			result = trie_result;
 		}
 		else
 		{
-			succeed = insert_bucket(key, value, (bucket_node*)current->m_left, &bucket_result);
+			succeed = insert_bucket(key, value, right_bucket_node, &bucket_result);
 			result = bucket_result;
 		}
 		if (!succeed)
@@ -83,14 +89,36 @@ bool hash_trie::insert_trie(const char* key, int value, int hash, int num_bits, 
 			return false;
 		}
 		*ppResult = new trie_node();
-		(*ppResult)->m_left = result;
-		(*ppResult)->m_right = current->m_right;
+		(*ppResult)->m_left = left_trie_node;
+		(*ppResult)->m_right = result;
 	}
+
 	return true;
 }
 
 bool hash_trie::insert_bucket(const char* key, int value, bucket_node* current, bucket_node** ppResult)
 {
+	if (current == nullptr)
+	{
+		*ppResult = new bucket_node(key, value);
+		return true;
+	}
+	else if (strcmp(current->m_key, key) == 0)
+	{
+		return false;
+	}
+	else
+	{
+		bucket_node* result;
+		bool succeed = this->insert_bucket(key, value, current->m_next, &result);
+		if (!succeed)
+		{
+			return false;
+		}
+		*ppResult = new bucket_node(current->m_key, value);
+		(*ppResult)->m_next = result;
+	}
+	return true;
 }
 
 hash_trie::node::~node()
--- data_structures/hash_trie_test.cpp
+++ data_structures/hash_trie_test.cpp
@@ -1,8 +1,11 @@
 #include "hash_trie_test.h"
 #include "hash_trie.h"
+#include <iostream>
+using namespace std;
 
 void hash_trie_test()
 {
 	hash_trie map;
 	map.set("ab", 1);
+	cout << "Done" << endl;
 }
