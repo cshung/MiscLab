8a53e6735cab362a22c33568a471ae0eecbdf8e1
Andrew Au <cshung@gmail.com>
2017-01-15 16:15:52 -0800

    Progress on btree insertion node splitting

--- MingDatabase/btree.cpp
+++ MingDatabase/btree.cpp
@@ -1,20 +1,53 @@
 #include "btree.h"
 
 #include <map>
+#include <vector>
 using namespace std;
+
+// TODO: For ease to debug, start with a simple 2-3 tree
+const int max_size = 3;
+const int min_size = 2;
+
+class btree_node;
+
+struct insert_result
+{
+    bool succeed;
+    bool split;
+};
+
+struct split_result
+{
+    btree_node* sibling;
+    int key;
+};
 
 class btree_node
 {
 public:
     virtual ~btree_node();
-    virtual btree_node* insert(int key, int value) = 0;
+    virtual insert_result insert(int key, int value) = 0;
+    virtual split_result split() = 0;
+};
+
+class btree_internal_node : public btree_node
+{
+public:
+    btree_internal_node(int key, btree_node* left, btree_node* right);
+    ~btree_internal_node();
+    virtual insert_result insert(int key, int value);
+    virtual split_result split();
+private:
+    vector<int> keys;
+    vector<btree_node*> children;
 };
 
 class btree_leaf_node : public btree_node
 {
 public:
     virtual ~btree_leaf_node();
-    virtual btree_node* insert(int key, int value);
+    virtual insert_result insert(int key, int value);
+    virtual split_result split();
 private:
     // TODO: Ideally, this should work on the memory page directly
     map<int, int> entries;
@@ -51,19 +84,100 @@ btree_leaf_node::~btree_leaf_node()
 {
 }
 
-btree_node* btree_leaf_node::insert(int key, int value)
+insert_result btree_leaf_node::insert(int key, int value)
 {
+    insert_result result;
+
     auto probe = this->entries.find(key);
     if (probe == this->entries.end())
     {
-        // Assuming the node is not full
+        result.succeed = true;
         this->entries.insert(pair<int, int>(key, value));
-        return this;
+        result.split = (entries.size() == max_size + 1);
+        return result;
     }
     else
     {
-        return nullptr;
+        result.succeed = false;
+        return result;
     }
+}
+
+split_result btree_leaf_node::split()
+{
+    split_result result;
+    btree_leaf_node* sibling = new btree_leaf_node();
+    int i = 0;
+    for (auto iter = this->entries.begin(); i < min_size; iter++, i++)
+    {
+        sibling->entries.insert(*iter);
+    }
+    for (auto iter = sibling->entries.begin(); iter != sibling->entries.end(); iter++)
+    {
+        this->entries.erase(iter->first);
+    }
+
+    result.sibling = sibling;
+    result.key = this->entries.begin()->first;
+    return result;
+}
+
+btree_internal_node::btree_internal_node(int key, btree_node* left, btree_node* right)
+{
+    this->keys.push_back(key);
+    this->children.push_back(left);
+    this->children.push_back(right);
+}
+
+btree_internal_node::~btree_internal_node()
+{
+
+}
+
+insert_result btree_internal_node::insert(int key, int value)
+{
+    insert_result result;
+    for (int upper_index = 0; upper_index <= this->keys.size(); upper_index++)
+    {
+        int lower_index = upper_index - 1;
+        bool lower_is_good = (lower_index == -1) || (this->keys[lower_index] <= key);
+        bool upper_is_good = (upper_index == this->keys.size()) || (key < this->keys[upper_index]);
+        if (lower_is_good && upper_is_good)
+        {
+            insert_result children_insert_result = this->children[upper_index]->insert(key, value);
+            if (children_insert_result.succeed)
+            {
+                result.succeed = true;
+                if (children_insert_result.split)
+                {
+                    split_result children_split_result = this->children[upper_index]->split();
+                    this->children.insert(this->children.begin() + upper_index, children_split_result.sibling);
+                    this->keys.insert(this->keys.begin() + upper_index, children_split_result.key);
+                    result.split = this->keys.size() == max_size;
+                }
+                else
+                {
+                    result.split = false;
+                }
+            }
+            else
+            {
+                result.succeed = false;
+            }
+
+            break;
+        }
+    }
+
+    return result;
+}
+
+split_result btree_internal_node::split()
+{
+    // TODO: Implementation
+    split_result result;
+    result.key = 0;
+    return result;
 }
 
 bool btree_impl::insert(int key, int value)
@@ -73,15 +187,24 @@ bool btree_impl::insert(int key, int value)
         this->m_root = new btree_leaf_node();
     }
 
-    btree_node* result = this->m_root->insert(key, value);
+    insert_result result = this->m_root->insert(key, value);
 
-    if (result == nullptr)
+    if (result.succeed)
     {
-        return false;
+        if (result.split)
+        {
+            split_result split_result = this->m_root->split();
+            this->m_root = new btree_internal_node(split_result.key, split_result.sibling, this->m_root);
+
+            return true;
+        }
+        else
+        {
+            return true;
+        }
     }
     else
     {
-        this->m_root = result;
-        return true;
+        return false;
     }
 }
--- MingDatabase/program.cpp
+++ MingDatabase/program.cpp
@@ -7,6 +7,13 @@ int main(int argc, char** argv)
 {
     btree index;
     cout << index.insert(1, 2) << endl;
+    cout << index.insert(2, 2) << endl;
+    cout << index.insert(3, 2) << endl;
+    cout << index.insert(4, 2) << endl;
+    cout << index.insert(5, 2) << endl;
+    cout << index.insert(6, 2) << endl;
+    cout << index.insert(7, 2) << endl;
+    cout << index.insert(8, 2) << endl;
     cout << "Hello World" << endl;
     return 0;
 }
