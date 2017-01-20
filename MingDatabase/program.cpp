2743329c919568a50824854e252c829b5c792e17
Andrew Au <cshung@gmail.com>
2017-01-19 19:11:24 -0800

    Randomized testing found interesting access violation

--- MingDatabase/program.cpp
+++ MingDatabase/program.cpp
@@ -1,23 +1,72 @@
 #include <iostream>
+#include <map>
+#include <cstdlib>
+#include <cassert>
 #include "btree.h"
 
 using namespace std;
 
 int main(int argc, char** argv)
 {
+    map<int, int> reference;
+    btree tree;
+    srand(0);
+    int min_key = -30;
+    int max_key = 30;
+    for (int i = 0; i < 1000; i++)
     {
-        btree index;
-        for (int i = 0; i < 8; i++)
+        int operation = rand() % 3;
+        switch (operation)
         {
-            index.insert(8 - i, i + 1);
-            index.print();
-            cout << "====================" << endl;
+        case 0:
+        {
+            int key_to_insert = rand() % (max_key - min_key) + min_key;
+            int val_to_insert = rand() % (max_key - min_key) + min_key;
+            bool insertion_result = tree.insert(key_to_insert, val_to_insert);
+            if (reference.find(key_to_insert) == reference.end())
+            {
+                reference.insert(make_pair(key_to_insert, val_to_insert));
+                assert(insertion_result);
+            }
+            else
+            {
+                assert(!insertion_result);
+            }
         }
+        break;
+        case 1:
+        {
+            int key_to_remove = rand() % (max_key - min_key) + min_key;
+            bool remove_result = tree.remove(key_to_remove);
+            if (reference.find(key_to_remove) != reference.end())
+            {
+                reference.erase(key_to_remove);
+                assert(remove_result);
+            }
+            else
+            {
+                assert(!remove_result);
+            }
+        }
+        break;
+        case 2:
+        {
+            int select_answer;
+            int key_to_select = rand() % (max_key - min_key) + min_key;
+            bool select_result = tree.select(key_to_select, &select_answer);
+            if (reference.find(key_to_select) != reference.end())
+            {
+                assert(select_result);
+                assert(reference[key_to_select] == select_answer);
+            }
+            else
+            {
+                assert(!select_result);
+            }
 
-        index.remove(5);
-
-        cout << "After remove" << endl;
-        index.print();
+            break;
+        }
+        }
 
     }
 
