7039e237b12349bf125f0d68eebbdf50a815538d
Andrew Au <cshung@gmail.com>
2017-02-25 17:12:13 -0800

    Recursion Theorem

---
+++ Recursion/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main()
+    {
+        string tape = @"        string a = @""class Program
+{
+    static void Main()
+    {
+        string tape = @"""""" + tape.Replace(""\"""", ""\""\"""") + @"""""";
+"";
+        System.Console.WriteLine(a + tape);
+    }
+}";
+        string a = @"class Program
+{
+    static void Main()
+    {
+        string tape = @""" + tape.Replace("\"", "\"\"") + @""";
+";
+        System.Console.WriteLine(a + tape);
+    }
+}
