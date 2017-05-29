c371bbcc3bf56df51bf8a46ceb77f5b35f7d0597
Andrew Au <cshung@gmail.com>
2017-05-29 16:39:30 -0700

    Tab to space

--- Burnside/Source.cpp
+++ Burnside/Source.cpp
@@ -48,133 +48,133 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-	vector<vector<int>> group_elements;
-	int n = 5;
-	int c = 10; // Polya's enumeration work much better when c is large - enumerating the configuration space is expensive
-	for (int offset = 0; offset < n; offset++)
-	{
-		vector<int> forward;
-		for (int i = 0; i < n; i++)
-		{
-			forward.push_back((i + offset) % n);
-		}
-		group_elements.push_back(forward);
-		vector<int> backward;
-		for (int i = 0; i < n; i++)
-		{
-			backward.push_back(((n - i) + offset) % n);
-		}
-		group_elements.push_back(backward);
-	}
+    vector<vector<int>> group_elements;
+    int n = 5;
+    int c = 10; // Polya's enumeration work much better when c is large - enumerating the configuration space is expensive
+    for (int offset = 0; offset < n; offset++)
+    {
+        vector<int> forward;
+        for (int i = 0; i < n; i++)
+        {
+            forward.push_back((i + offset) % n);
+        }
+        group_elements.push_back(forward);
+        vector<int> backward;
+        for (int i = 0; i < n; i++)
+        {
+            backward.push_back(((n - i) + offset) % n);
+        }
+        group_elements.push_back(backward);
+    }
 
-	bool burnside = true;
-	bool polya = true;
+    bool burnside = true;
+    bool polya = true;
 
-	cout << "Begin Group" << endl;
-	for (size_t i = 0; i < group_elements.size(); i++)
-	{
-		for (size_t j = 0; j < group_elements[i].size(); j++)
-		{
-			cout << group_elements[i][j] << " ";
-		}
-		cout << endl;
-	}
-	cout << "End Group" << endl;
-	if (burnside)
-	{
-		cout << "Starting brute force verification of Burnside's lemma" << endl;
-		// This version of the code directly exercise the Burnside's lemma as is:
-		// This is inefficient because we have to go through all configurations
-		vector<int> config;
-		vector<int> mirror;
-		config.resize(n);
-		mirror.resize(n);
+    cout << "Begin Group" << endl;
+    for (size_t i = 0; i < group_elements.size(); i++)
+    {
+        for (size_t j = 0; j < group_elements[i].size(); j++)
+        {
+            cout << group_elements[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << "End Group" << endl;
+    if (burnside)
+    {
+        cout << "Starting brute force verification of Burnside's lemma" << endl;
+        // This version of the code directly exercise the Burnside's lemma as is:
+        // This is inefficient because we have to go through all configurations
+        vector<int> config;
+        vector<int> mirror;
+        config.resize(n);
+        mirror.resize(n);
 
-		int all_stablizer_count = 0;
-		for (size_t k = 0; k < group_elements.size(); k++)
-		{
-			for (size_t j = 0; j < group_elements[k].size(); j++)
-			{
-				cout << group_elements[k][j] << " ";
-			}
-			int fixed_count = 0;
-			int seq = 0;
+        int all_stablizer_count = 0;
+        for (size_t k = 0; k < group_elements.size(); k++)
+        {
+            for (size_t j = 0; j < group_elements[k].size(); j++)
+            {
+                cout << group_elements[k][j] << " ";
+            }
+            int fixed_count = 0;
+            int seq = 0;
 
-			// A simple odometer to loop through all the config
-			while (true)
-			{
-				int cur = seq;
-				bool last = true;
-				for (int i = 0; i < n; i++)
-				{
-					config[i] = cur % c;
-					cur = cur / c;
-					last = last & (config[i] == c - 1);
-				}
+            // A simple odometer to loop through all the config
+            while (true)
+            {
+                int cur = seq;
+                bool last = true;
+                for (int i = 0; i < n; i++)
+                {
+                    config[i] = cur % c;
+                    cur = cur / c;
+                    last = last & (config[i] == c - 1);
+                }
 
-				// Here we have got a configuration - check if it is fixed by the current permutation
+                // Here we have got a configuration - check if it is fixed by the current permutation
 
-				bool is_fixed = true;
-				for (int l = 0; is_fixed && l < n; l++)
-				{
-					is_fixed = is_fixed && (config[group_elements[k][l]] == config[l]);
-				}
+                bool is_fixed = true;
+                for (int l = 0; is_fixed && l < n; l++)
+                {
+                    is_fixed = is_fixed && (config[group_elements[k][l]] == config[l]);
+                }
 
-				if (is_fixed)
-				{
-					fixed_count++;
-				}
+                if (is_fixed)
+                {
+                    fixed_count++;
+                }
 
-				if (last)
-				{
-					break;
-				}
-				seq++;
-			}
-			cout << " fixes " << fixed_count << " configurations" << endl;
-			all_stablizer_count += fixed_count;
-		}
-		cout << all_stablizer_count << " " << group_elements.size() << endl;
-	}
+                if (last)
+                {
+                    break;
+                }
+                seq++;
+            }
+            cout << " fixes " << fixed_count << " configurations" << endl;
+            all_stablizer_count += fixed_count;
+        }
+        cout << all_stablizer_count << " " << group_elements.size() << endl;
+    }
 
-	if (polya)
-	{
-		cout << "Starting polya's enumeration formula" << endl;
-		int all_stablizer_count = 0;
-		for (size_t i = 0; i < group_elements.size(); i++)
-		{
-			vector<bool> used;
-			used.resize(group_elements[i].size());
-			for (size_t j = 0; j < used.size(); j++)
-			{
-				used[j] = false;
-			}
-			for (size_t j = 0; j < group_elements[i].size(); j++)
-			{
-				cout << group_elements[i][j] << " ";
-			}
-			int fixed_count = 1;
-			for (size_t j = 0; j < group_elements[i].size(); j++)
-			{
-				size_t k = j;
-				int element_in_cycle = 0;
-				cout << "(";
-				while (!used[k])
-				{
-					element_in_cycle++;
-					cout << k << " ";
-					used[k] = true;
-					k = group_elements[i][k];
-				}
-				cout << ") [" << element_in_cycle << "] ";
-				if (element_in_cycle != 0)
-				{
-					fixed_count *= c;
-				}
-			}
-			cout << " fixes " << fixed_count << " configurations" << endl;
-			all_stablizer_count += fixed_count;
-		}
-		cout << all_stablizer_count << " " << group_elements.size() << endl;
-	}
+    if (polya)
+    {
+        cout << "Starting polya's enumeration formula" << endl;
+        int all_stablizer_count = 0;
+        for (size_t i = 0; i < group_elements.size(); i++)
+        {
+            vector<bool> used;
+            used.resize(group_elements[i].size());
+            for (size_t j = 0; j < used.size(); j++)
+            {
+                used[j] = false;
+            }
+            for (size_t j = 0; j < group_elements[i].size(); j++)
+            {
+                cout << group_elements[i][j] << " ";
+            }
+            int fixed_count = 1;
+            for (size_t j = 0; j < group_elements[i].size(); j++)
+            {
+                size_t k = j;
+                int element_in_cycle = 0;
+                cout << "(";
+                while (!used[k])
+                {
+                    element_in_cycle++;
+                    cout << k << " ";
+                    used[k] = true;
+                    k = group_elements[i][k];
+                }
+                cout << ") [" << element_in_cycle << "] ";
+                if (element_in_cycle != 0)
+                {
+                    fixed_count *= c;
+                }
+            }
+            cout << " fixes " << fixed_count << " configurations" << endl;
+            all_stablizer_count += fixed_count;
+        }
+        cout << all_stablizer_count << " " << group_elements.size() << endl;
+    }
 }
