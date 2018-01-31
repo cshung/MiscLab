b4fba466c94ad05067bfe2d9f2c5d9617eede258
Andrew Au <andrewau@microsoft.com>
2018-01-30 18:26:31 -0800

    Getting most construction state on the stack

--- data_structures/suffix_tree.cpp
+++ data_structures/suffix_tree.cpp
@@ -22,57 +22,11 @@ public:
     suffix_tree_impl(string s);
     ~suffix_tree_impl();
 private:
-    void search(int start, int end);
-    bool add(int start, int end);
-    char first(node* node);
-    int length(node* node);
+    char first_char(node* node);
+    int length(node* node, int m_end);
     string m_s;
-
     node* m_root;
-    node* m_node_cursor;
-    int m_edge_cursor;
-
-    node* m_next_node_cursor;
-    int m_next_text_cursor;
-
-    int m_end;
-
-    node* m_last_internal_node;
-    void print(int indent, node* node);
 };
-
-void suffix_tree_impl::print(int indent, node* n)
-{
-    if (n == nullptr)
-    {
-        return;
-    }
-    for (int i = 0; i < indent; i++)
-    {
-        cout << "-";
-    }
-    if (n->m_first_child == nullptr)
-    {
-        for (int i = n->m_begin; i < this->m_end; i++)
-        {
-            cout << this->m_s[i];
-        }
-    }
-    else
-    {
-        for (int i = n->m_begin; i < n->m_end; i++)
-        {
-            cout << this->m_s[i];
-        }
-    }
-    cout << endl;
-    node* child = n->m_first_child;
-    while (child != nullptr)
-    {
-        this->print(indent + 1, child);
-        child = child->m_sibling;
-    }
-}
 
 node::node() : m_begin(0), m_end(0), m_parent(nullptr), m_first_child(nullptr), m_sibling(nullptr), m_suffix_link(nullptr)
 {
@@ -93,19 +47,167 @@ node::~node()
 
 suffix_tree_impl::suffix_tree_impl(string s) : m_s(s)
 {
+    node* m_node_cursor;
+    int m_edge_cursor;
+    node* m_next_node_cursor;
+    int m_next_text_cursor;
+    int m_end;
+    node* m_last_internal_node;
+
     this->m_root = new node();
-    this->m_node_cursor = nullptr;
-    this->m_next_node_cursor = nullptr;
-    this->m_last_internal_node = nullptr;
-    unsigned int start = 0;
-    for (unsigned int end = 1; end <= s.length(); end++)
+    m_node_cursor = nullptr;
+    m_next_node_cursor = nullptr;
+    m_last_internal_node = nullptr;
+    int start = 0;
+    for (int end = 1; end <= s.length(); end++)
     {
-        this->m_next_node_cursor = this->m_root;
-        this->m_next_text_cursor = start;
-        this->m_end = end;
+        m_next_node_cursor = this->m_root;
+        m_next_text_cursor = start;
+        m_end = end;
         for (; start < end; start++)
         {
-            if (this->add(start, end))
+            bool no_op_applied = false;
+
+            m_node_cursor = m_next_node_cursor;
+            start = m_next_text_cursor;
+            m_edge_cursor = 0;
+
+            int text_cursor = start;
+            m_next_node_cursor = this->m_root;
+            m_next_text_cursor = start + 1;
+            while (text_cursor < end - 1)
+            {
+                int node_length = length(m_node_cursor, m_end);
+                if (m_edge_cursor == node_length)
+                {
+                    if (m_node_cursor->m_suffix_link != nullptr)
+                    {
+                        m_next_node_cursor = m_node_cursor->m_suffix_link;
+                        m_next_text_cursor = text_cursor - 1;
+                    }
+
+                    char next_char = this->m_s[text_cursor];
+                    node* child_cursor = m_node_cursor->m_first_child;
+                    while (true)
+                    {
+                        assert(child_cursor != nullptr);
+                        if (this->first_char(child_cursor) == next_char)
+                        {
+                            m_node_cursor = child_cursor;
+                            m_edge_cursor = 0;
+                            break;
+                        }
+                        else
+                        {
+                            child_cursor = child_cursor->m_sibling;
+                        }
+                    }
+                }
+                else
+                {
+                    int text_move = end - 1 - text_cursor;
+                    int edge_move = node_length - m_edge_cursor;
+                    int move = text_move > edge_move ? edge_move : text_move;
+                    m_edge_cursor += move;
+                    text_cursor += move;
+                }
+            }
+
+            char next_text_char = this->m_s[end - 1];
+            node* search_end = nullptr;
+            node* new_internal_node = nullptr;
+            if (m_edge_cursor == length(m_node_cursor, m_end))
+            {
+                if (m_node_cursor != this->m_root && m_node_cursor->m_first_child == nullptr)
+                {
+                }
+                else
+                {
+                    node* search = m_node_cursor->m_first_child;
+                    bool found = false;
+                    while (search != nullptr)
+                    {
+                        if (first_char(search) == next_text_char)
+                        {
+                            found = true;
+                            break;
+                        }
+                        else
+                        {
+                            search = search->m_sibling;
+                        }
+                    }
+                    if (found)
+                    {
+                        no_op_applied = true;
+                    }
+                    else
+                    {
+                        node* new_leaf = new node();
+                        new_leaf->m_begin = end - 1;
+                        new_leaf->m_parent = m_node_cursor;
+                        new_leaf->m_sibling = m_node_cursor->m_first_child;
+                        m_node_cursor->m_first_child = new_leaf;
+                    }
+                }
+                search_end = m_node_cursor;
+            }
+            else
+            {
+                char next_tree_char = this->m_s[m_node_cursor->m_begin + m_edge_cursor];
+                if (next_text_char == next_tree_char)
+                {
+                    no_op_applied = true;
+                }
+                else
+                {
+                    node* new_node = new node();
+                    node* new_leaf = new node();
+                    new_leaf->m_begin = end - 1;
+                    new_node->m_begin = m_node_cursor->m_begin;
+                    new_node->m_end = m_node_cursor->m_begin + m_edge_cursor;
+                    m_node_cursor->m_begin = m_node_cursor->m_begin + m_edge_cursor;
+
+                    new_node->m_parent = m_node_cursor->m_parent;
+                    new_leaf->m_parent = new_node;
+                    m_node_cursor->m_parent = new_node;
+
+                    new_node->m_sibling = new_node->m_parent->m_first_child;
+                    new_node->m_parent->m_first_child = new_node;
+
+                    node* search = new_node;
+                    while (search != nullptr)
+                    {
+                        if (search->m_sibling == m_node_cursor)
+                        {
+                            search->m_sibling = search->m_sibling->m_sibling;
+                            break;
+                        }
+                        search = search->m_sibling;
+                    }
+
+                    new_node->m_first_child = new_leaf;
+                    new_leaf->m_sibling = m_node_cursor;
+                    m_node_cursor->m_sibling = nullptr;
+
+                    new_internal_node = search_end = new_node;
+                }
+            }
+
+            if (m_last_internal_node != nullptr)
+            {
+                assert(m_last_internal_node->m_suffix_link == nullptr);
+                assert(search_end != nullptr);
+                m_last_internal_node->m_suffix_link = search_end;
+                m_last_internal_node = nullptr;
+            }
+
+            if (new_internal_node != nullptr)
+            {
+                m_last_internal_node = new_internal_node;
+            }
+
+            if (no_op_applied)
             {
                 break;
             }
@@ -113,7 +215,7 @@ suffix_tree_impl::suffix_tree_impl(string s) : m_s(s)
     }
 }
 
-int suffix_tree_impl::length(node* node)
+int suffix_tree_impl::length(node* node, int m_end)
 {
     if (node == this->m_root)
     {
@@ -121,7 +223,7 @@ int suffix_tree_impl::length(node* node)
     }
     else if (node->m_first_child == nullptr)
     {
-        return this->m_end - node->m_begin;
+        return m_end - node->m_begin;
     }
     else
     {
@@ -129,163 +231,9 @@ int suffix_tree_impl::length(node* node)
     }
 }
 
-char suffix_tree_impl::first(node* node)
+char suffix_tree_impl::first_char(node* node)
 {
     return this->m_s[node->m_begin];
-}
-
-void suffix_tree_impl::search(int start, int end)
-{
-    int text_cursor = start;
-    this->m_next_node_cursor = this->m_root;
-    this->m_next_text_cursor = start + 1;
-    while (text_cursor < end)
-    {
-        int node_length = length(this->m_node_cursor);
-        if (this->m_edge_cursor == node_length)
-        {
-            if (this->m_node_cursor->m_suffix_link != nullptr)
-            {
-                this->m_next_node_cursor = this->m_node_cursor->m_suffix_link;
-                this->m_next_text_cursor = text_cursor - 1;
-            }
-
-            char next_char = this->m_s[text_cursor];
-            node* child_cursor = this->m_node_cursor->m_first_child;
-            while (true)
-            {
-                assert(child_cursor != nullptr);
-                if (this->first(child_cursor) == next_char)
-                {
-                    this->m_node_cursor = child_cursor;
-                    this->m_edge_cursor = 0;
-                    break;
-                }
-                else
-                {
-                    child_cursor = child_cursor->m_sibling;
-                }
-            }
-        }
-        else
-        {
-            int text_move = end - text_cursor;
-            int edge_move = node_length - this->m_edge_cursor;
-            int move = text_move > edge_move ? edge_move : text_move;
-            this->m_edge_cursor += move;
-            text_cursor += move;
-        }
-    }
-}
-
-bool suffix_tree_impl::add(int start, int end)
-{
-    cout << "Add " << start << "," << end << endl;
-    bool no_op_applied = false;
-
-    this->m_node_cursor = this->m_next_node_cursor;
-    start = this->m_next_text_cursor;
-    this->m_edge_cursor = 0;
-
-    this->search(start, end - 1);
-
-    char next_text_char = this->m_s[end - 1];
-    node* search_end = nullptr;
-    node* new_internal_node = nullptr;
-    if (this->m_edge_cursor == length(this->m_node_cursor))
-    {
-        if (this->m_node_cursor != this->m_root && this->m_node_cursor->m_first_child == nullptr)
-        {
-        }
-        else
-        {
-            node* search = this->m_node_cursor->m_first_child;
-            bool found = false;
-            while (search != nullptr)
-            {
-                if (first(search) == next_text_char)
-                {
-                    found = true;
-                    break;
-                }
-                else
-                {
-                    search = search->m_sibling;
-                }
-            }
-            if (found)
-            {
-                no_op_applied = true;
-            }
-            else
-            {
-                node* new_leaf = new node();
-                new_leaf->m_begin = end - 1;
-                new_leaf->m_parent = m_node_cursor;
-                new_leaf->m_sibling = m_node_cursor->m_first_child;
-                this->m_node_cursor->m_first_child = new_leaf;
-            }
-        }
-        search_end = this->m_node_cursor;
-    }
-    else
-    {
-        char next_tree_char = this->m_s[this->m_node_cursor->m_begin + this->m_edge_cursor];
-        if (next_text_char == next_tree_char)
-        {
-            no_op_applied = true;
-        }
-        else
-        {
-            node* new_node = new node();
-            node* new_leaf = new node();
-            new_leaf->m_begin = end - 1;
-            new_node->m_begin = this->m_node_cursor->m_begin;
-            new_node->m_end = this->m_node_cursor->m_begin + this->m_edge_cursor;
-            this->m_node_cursor->m_begin = this->m_node_cursor->m_begin + this->m_edge_cursor;
-
-            new_node->m_parent = this->m_node_cursor->m_parent;
-            new_leaf->m_parent = new_node;
-            this->m_node_cursor->m_parent = new_node;
-
-            new_node->m_sibling = new_node->m_parent->m_first_child;
-            new_node->m_parent->m_first_child = new_node;
-
-            node* search = new_node;
-            while (search != nullptr)
-            {
-                if (search->m_sibling == this->m_node_cursor)
-                {
-                    search->m_sibling = search->m_sibling->m_sibling;
-                    break;
-                }
-                search = search->m_sibling;
-            }
-
-            new_node->m_first_child = new_leaf;
-            new_leaf->m_sibling = this->m_node_cursor;
-            this->m_node_cursor->m_sibling = nullptr;
-
-            new_internal_node = search_end = new_node;
-        }
-    }
-
-    if (this->m_last_internal_node != nullptr)
-    {
-        assert(this->m_last_internal_node->m_suffix_link == nullptr);
-        assert(search_end != nullptr);
-        this->m_last_internal_node->m_suffix_link = search_end;
-        this->m_last_internal_node = nullptr;
-    }
-
-    if (new_internal_node != nullptr)
-    {
-        this->m_last_internal_node = new_internal_node;
-    }
-
-    print(1, this->m_root);
-
-    return no_op_applied;
 }
 
 suffix_tree_impl::~suffix_tree_impl()
