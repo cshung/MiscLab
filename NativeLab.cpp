d8edf6c8780a369eec80b407f4d42fa85da15215
Andrew Au <cshung@gmail.com>
2020-10-11 22:47:17 -0700

    Saving some prototype code

---
+++ NativeLab.cpp
@@ -0,0 +1,177 @@
+#include <bitset>
+#include <iostream>
+#include <cassert>
+#include <vector>
+#include <stack>
+#include <map>
+#include <queue>
+#include <algorithm>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+
+/*
+
+GF(2n), to be productized
+
+void print(int i)
+{
+    // bitset<8> x(i);
+    // cout << x << " ";
+    cout << i << " ";
+}
+
+int shift(int i)
+{
+    i = i << 1;
+    if ((i & (1 << 3)) != 0)
+    {
+        // 76543210
+        // 00001011
+        i = i ^ 0xb;
+    }
+    return i;
+}
+
+int add(int i, int j)
+{
+    return i ^ j;
+}
+
+int multiply(int i, int j)
+{
+    int result = 0;
+    int mask = 1;
+    for (int bit = 0; bit < 3; bit++)
+    {
+        if ((mask & j) != 0)
+        {
+            int temp = i;
+            for (int times = 0; times < bit; times++)
+            {
+                temp = shift(temp);
+            }
+            result = add(result, temp);
+        }
+        mask = mask << 1;
+    }
+    return result;
+}
+
+int main()
+{
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            cout << multiply(i, j) << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+*/
+
+const int n = 64;
+const int a = 125;
+const int prime = 193;
+const int inverse = 190;
+int powers[64];
+
+int mod(int x, int p)
+{
+    return ((x % p) + p) % p;
+}
+
+void number_theoretic_transform_helper(int n, int* input, int* output, int sign)
+{
+    for (int f = 0; f < n; f++)
+    {
+        output[f] = 0;
+        for (int t = 0; t < n; t++)
+        {
+            output[f] = mod(output[f] + mod(powers[mod(sign * t * f, n)] * input[t], prime), prime);
+        }
+    }
+}
+
+void number_theoretic_transform(int n, int* input, int* output)
+{
+    number_theoretic_transform_helper(n, input, output, -1);
+}
+
+void inverse_number_theoretic_transform(int n, int* input, int* output)
+{
+    number_theoretic_transform_helper(n, input, output, 1);
+    for (int f = 0; f < n; f++)
+    {
+        output[f] = mod(output[f] * inverse, prime);
+    }
+}
+
+void fast_number_theoretic_transform_helper(int length, int input_offset, int stride, int* input, int output_offset, int* output, int sign)
+{
+    if (length == 1)
+    {
+        output[output_offset] = input[input_offset];
+    }
+    else
+    {
+        int half = length / 2;
+        fast_number_theoretic_transform_helper(half, input_offset, stride * 2, input, output_offset, output, sign);
+        fast_number_theoretic_transform_helper(half, input_offset + stride, stride * 2, input, output_offset + half, output, sign);
+        for (int i = 0; i < half; i++)
+        {
+            int a = output[output_offset + i];
+            int b = output[output_offset + i + half];
+            int c = mod(a + mod(powers[mod(-i * n / length * sign, n)] * b, prime), prime);
+            int d = mod(a + mod(powers[mod((half - i) * n / length * sign, n)] * b, prime), prime);
+            output[output_offset + i] = c;
+            output[output_offset + (i + half)] = d;
+        }
+    }
+}
+
+void fast_number_theoretic_transform(int length, int input_offset, int stride, int* input, int output_offset, int* output)
+{
+    fast_number_theoretic_transform_helper(length, input_offset, stride, input, output_offset, output, 1);
+}
+
+void inverse_fast_number_theoretic_transform(int length, int input_offset, int stride, int* input, int output_offset, int* output)
+{
+    fast_number_theoretic_transform_helper(length, input_offset, stride, input, output_offset, output, -1);
+    for (int f = 0; f < n; f++)
+    {
+        output[output_offset + f] = mod(output[output_offset + f] * inverse, prime);
+    }
+}
+
+int main(int argc, char** argv)
+{
+    int p = 1;
+    for (int i = 0; i < n; i++)
+    {
+        powers[i] = p;
+        p = mod(p * a, prime);
+    }
+    int input[64];
+    int output[64];
+    for (int i = 0; i < 32; i++)
+    {
+        input[i] = 1;
+        input[i + 32] = 0;
+    }
+    fast_number_theoretic_transform(64, 0, 1, input, 0, output);
+    for (int i = 0; i < 64; i++)
+    {
+        // output[i] = mod(output[i] * output[i], prime);
+        cout << output[i] << " ";
+    }
+    cout << endl;
+    inverse_fast_number_theoretic_transform(64, 0, 1, output, 0, input);
+    for (int i = 0; i < 64; i++)
+    {
+        cout << input[i] << " ";
+    }
+    return 0;
+}
