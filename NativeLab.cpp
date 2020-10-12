347cf224d6217d58dc6e35b8a67a2cb6ee936ef0
Andrew Au <cshung@gmail.com>
2020-10-12 10:31:19 -0700

    More temp changes

--- NativeLab.cpp
+++ NativeLab.cpp
@@ -74,6 +74,7 @@ int main()
 
 const int n = 64;
 const int a = 125;
+// TODO, prime should account for largest possible overlap, good for now
 const int prime = 193;
 const int inverse = 190;
 int powers[64];
@@ -109,7 +110,8 @@ void inverse_number_theoretic_transform(int n, int* input, int* output)
     }
 }
 
-void fast_number_theoretic_transform_helper(int length, int input_offset, int stride, int* input, int output_offset, int output_stride, int* output, int sign)
+
+void fast_number_theoretic_transform_helper(int length, int input_offset, int stride, vector<int>& input, int output_offset, int output_stride, vector<int>& output, int sign)
 {
     if (length == 1)
     {
@@ -132,21 +134,22 @@ void fast_number_theoretic_transform_helper(int length, int input_offset, int st
     }
 }
 
-void fast_number_theoretic_transform(int length, int input_offset, int stride, int* input, int output_offset, int output_stride, int* output)
+void fast_number_theoretic_transform(int length, int input_offset, int stride, vector<int>& input, int output_offset, int output_stride, vector<int>& output)
 {
     fast_number_theoretic_transform_helper(length, input_offset, stride, input, output_offset, output_stride, output, 1);
 }
 
-void inverse_fast_number_theoretic_transform(int length, int input_offset, int stride, int* input, int output_offset, int output_stride, int* output)
+void inverse_fast_number_theoretic_transform(int length, int input_offset, int stride, vector<int>& input, int output_offset, int output_stride, vector<int>& output)
 {
     fast_number_theoretic_transform_helper(length, input_offset, stride, input, output_offset, output_stride, output, -1);
-    for (int f = 0; f < n; f++)
+    for (int f = 0; f < length; f++)
     {
+        // TODO - What I really needed here is the multiplicative inverse of length, not the multiplicative inverse of 64
         output[output_offset + f * output_stride] = mod(output[output_offset + f * output_stride] * inverse, prime);
     }
 }
 
-int main(int argc, char** argv)
+void init_powers()
 {
     int p = 1;
     for (int i = 0; i < n; i++)
@@ -154,24 +157,91 @@ int main(int argc, char** argv)
         powers[i] = p;
         p = mod(p * a, prime);
     }
-    int input[128];
-    int output[128];
-    for (int i = 0; i < 32; i++)
+}
+
+/*
+clc;clear;
+it1 = [1 1 0;0 1 0;0 1 0];
+it2 = [1 0 0;1 1 0;0 0 0];
+IT1 = [it1 zeros(3, 3);zeros(3, 6)];
+IT2 = [it2 zeros(3, 3);zeros(3, 6)];
+IF1 = fft2(IT1);
+IF2 = fft2(IT2);
+CF = IF1 .* IF2;
+CT = ifft2(CF)
+conv2(it1,it2)
+ */
+int main(int argc, char** argv)
+{
+    init_powers();
+    vector<vector<int>> img1(3, vector<int>(3, 0));
+    vector<vector<int>> img2(3, vector<int>(3, 0));
+    img1[0][0] = 1; img1[0][1] = 1; img1[0][2] = 0;
+    img1[1][0] = 0; img1[1][1] = 1; img1[1][2] = 0;
+    img1[2][0] = 0; img1[2][1] = 1; img1[2][2] = 0;
+
+    img2[0][0] = 0; img2[0][1] = 0; img2[0][2] = 0;
+    img2[1][0] = 0; img2[1][1] = 1; img2[1][2] = 1;
+    img2[2][0] = 0; img2[2][1] = 0; img2[2][2] = 1;
+
+    vector<int> padded_image1(64, 0);
+    vector<int> padded_image2(64, 0);
+
+    vector<int> image1_rows(64, 0);
+    vector<int> image2_rows(64, 0);
+    vector<int> image1_rows_cols(64, 0);
+    vector<int> image2_rows_cols(64, 0);
+    vector<int> product (64, 0);
+    vector<int> product_rows(64, 0);
+    vector<int> product_rows_cols(64, 0);
+
+    for (int row = 0; row < img1.size(); row++)
     {
-        input[i * 2] = 1;
-        input[i * 2 + 64] = 0;
+        for (int col = 0; col < img1[0].size(); col++)
+        {
+            padded_image1[row * 8 + col] = img1[row][col];
+        }
     }
-    fast_number_theoretic_transform(64, 0, 2, input, 0, 2, output);
+
+    for (int row = 0; row < img2.size(); row++)
+    {
+        for (int col = 0; col < img2[0].size(); col++)
+        {
+            padded_image2[row * 8 + col] = img2[img2.size() - row - 1][img2[0].size() - col - 1];
+        }
+    }
+
+    for (int row = 0; row < 8; row++)
+    {
+        fast_number_theoretic_transform(8, row * 8, 1, padded_image1, row * 8, 1, image1_rows);
+        fast_number_theoretic_transform(8, row * 8, 1, padded_image2, row * 8, 1, image2_rows);
+    }
+    for (int col = 0; col < 8; col++)
+    {
+        fast_number_theoretic_transform(8, col, 8, image1_rows, col, 8, image1_rows_cols);
+        fast_number_theoretic_transform(8, col, 8, image2_rows, col, 8, image2_rows_cols);
+    }
     for (int i = 0; i < 64; i++)
     {
-        // output[i] = mod(output[i] * output[i], prime);
-        cout << output[i * 2] << " ";
+        product[i] = mod(image1_rows_cols[i] * image2_rows_cols[i], prime);
     }
-    cout << endl;
-    inverse_fast_number_theoretic_transform(64, 0, 2, output, 0, 2, input);
-    for (int i = 0; i < 64; i++)
+    for (int row = 0; row < 8; row++)
     {
-        cout << input[i * 2] << " ";
+        inverse_fast_number_theoretic_transform(8, row * 8, 1, product, row * 8, 1, product_rows);
     }
+    for (int col = 0; col < 8; col++)
+    {
+        inverse_fast_number_theoretic_transform(8, col, 8, product_rows, col, 8, product_rows_cols);
+    }
+    for (int row = 0; row < 8; row++)
+    {
+        for (int col = 0; col < 8; col++)
+        {
+            // TODO, eliminate this compensation
+            cout << mod(product_rows_cols[row * 8 + col] * 64, prime) << " ";
+        }
+        cout << endl;
+    }
+
     return 0;
 }
